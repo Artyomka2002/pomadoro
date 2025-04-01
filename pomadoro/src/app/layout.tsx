0b780395139d0fc0a254bd0c9bbae71aff8d3b3b
Artyomka2002 <ilyichev.a.a@mail.ru>
2025-04-01 20:42:58 +0700

    first commit

--- pomadoro/src/app/layout.tsx
+++ pomadoro/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono , } from "next/font/google";
+import { Rubik_Glitch } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +12,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
