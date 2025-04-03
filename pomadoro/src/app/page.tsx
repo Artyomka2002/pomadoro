d34db89bc92b43bfb38015956e36fae324e33062
Artyomka2002 <ilyichev.a.a@mail.ru>
2025-04-04 02:33:24 +0700

    add main screen

--- pomadoro/src/app/page.tsx
+++ pomadoro/src/app/page.tsx
@@ -1,5 +1,7 @@
-import { subscribe } from "diagnostics_channel";
+"use client";
+import { useState, useEffect } from "react";
 import { Rubik_Glitch } from "next/font/google";
+import { format } from "path";
 
 const rubik = Rubik_Glitch({
   weight: "400",
@@ -7,27 +9,139 @@ const rubik = Rubik_Glitch({
 });
 
 export default function Home() {
+  const [booleanTime, setBooleanTime] = useState(25 * 60);
+  const [stopTracker, setStopTreacker] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      setIsDarkMode(true);
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    const newTheme = !isDarkMode ? "dark" : "light";
+    setIsDarkMode(!isDarkMode);
+    document.documentElement.classList.toggle("dark", !isDarkMode);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  useEffect(() => {
+    if (!stopTracker) return;
+    const interval = setInterval(() => {
+      setBooleanTime((prevTime) => {
+        if (prevTime <= 0) {
+          clearInterval(interval);
+          return 0;
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [stopTracker]);
+
+  function formatTime(time: number) {
+    let min = Math.floor(time / 60);
+    let sec = time % 60;
+    return `${min}:${sec <= 10 ? "0" + sec : sec}`;
+  }
+
   return (
-    //   <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    //  asd
-    //   </div>
     <>
-      <header className="flex justify-between px-20 py-10">
-        <span className="flex gap-6 items-center">
-          <img
-            src="./pomadoroLogoimage.png"
-            alt="none"
-            className="h-[28px]"
-          ></img>
-          <div className={`${rubik.className} w-[79px] h-[26px]`}>404sq</div>
-        </span>
-        <span className="flex gap-4">
-          <button className="border-1 px-4 rounded-md">Sing Up</button>
-          <button className="border-1 px-4 rounded-md">Sing In</button>
-        </span>
-      </header>
-      <main></main>
-      <footer></footer>
+      <div className="flex flex-col h-screen justify-between">
+        <header className="flex justify-between px-20 py-10">
+          <span className="flex gap-6 items-center">
+           {isDarkMode ? '' :  <img
+              src="./pomadoroLogoimage.png"
+              alt="none"
+              className="h-[28px]"
+            /> }
+            <div className={`${rubik.className} w-[79px] h-[26px] text-lg`}>
+              404sq
+            </div>
+          </span>
+          <span className="flex gap-4">
+            <button
+              className="w-[108px] p-2 border-3 px-4 rounded-md cursor-pointer"
+              onClick={() => alert("Войти")}
+            >
+              Sign Up
+            </button>
+            <button
+              className="buttonSingIn"
+              onClick={() => alert("Зарегистрироваться")}
+            >
+              Sign In
+            </button>
+          </span>
+        </header>
+
+        <main className="flex justify-center items-center flex-col">
+          <div className="flex justify-center items-center">
+            <span className="text-[128px] font-cursive font-bold tracking-wider">
+              {formatTime(booleanTime)}
+            </span>
+          </div>
+          <div className="flex gap-5 mt-6">
+            <button
+              className="w-[152px] p-2 border-2 px-4 rounded-md hover:bg-gray-800 hover:text-gray-300 cursor-pointer"
+              onClick={() => setStopTreacker(false)}
+            >
+              Pause
+            </button>
+            <button className="cursor-pointer">
+              {isDarkMode ? (
+                <img
+                  src="reset-btn (1).svg"
+                  alt="reset"
+                  onClick={() => {
+                    setBooleanTime(25 * 60);
+                    setStopTreacker(false);
+                  }}
+                />
+              ) : (
+                <img
+                  src="reset-btn.svg "
+                  alt="reset"
+                  onClick={() => {
+                    setBooleanTime(25 * 60);
+                    setStopTreacker(false);
+                  }}
+                />
+              )}
+            </button>
+            <button
+              className="w-[152px] p-2 border-2 px-4 rounded-md hover:bg-gray-800 hover:text-gray-300 cursor-pointer"
+              onClick={() => setStopTreacker(true)}
+            >
+              Start
+            </button>
+          </div>
+        </main>
+        <footer className="flex justify-end p-4 ">
+          <span>
+            {isDarkMode ? (
+              <img
+                src="SVGRepo_iconCarrier.svg"
+                alt="theme"
+                className="cursor-pointer"
+                onClick={toggleTheme}
+              />
+            ) : (
+              <img
+                src="theme-btn.svg"
+                alt="theme"
+                className="cursor-pointer"
+                onClick={toggleTheme}
+              />
+            )}
+          </span>
+        </footer>
+      </div>
     </>
   );
 }
